023bc0da669d87df6e9291ad5712cfee951536cb
Artyom <artyom.santoyan.ait@gmail.com>
2025-09-24 12:10:09 +0400

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,28 +17,30 @@ const Footer = () => {
 
       <div className="footer-section">
         <h3>Կայքի բաժինները</h3>
-        <ul>
-          <li>Տան համար</li>
-          <li>Հատուկ առաջարկներ</li>
-          <li>All in փաթեթներ</li>
-          <li>Ինտերնետ</li>
-          <li>Հեռախոսակապ</li>
-          <li>Հեռախոսային սպասարկում</li>
-          <li>Գրասենյակներ և ծածկույթ</li>
-          <li>Միջազգային հավաստագրեր</li>
-          <li>Գաղտնիության քաղաքականություն</li>
-        </ul>
-        <ul>
-          <li>Բիզնեսի համար</li>
-          <li>Օպերատորներ</li>
-          <li>Օգնություն</li>
-          <li>Պայմաններ</li>
-          <li>Արխիվ</li>
-          <li>Աշխատատեղեր</li>
-          <li>Այլ հատուկ առաջարկներ</li>
-          <li>Այլ աշխատանքներ</li>
-          <li>Այլ</li>
-        </ul>
+        <div className="footer-section-1">
+          <ul>
+            <li>Տան համար</li>
+            <li>Հատուկ առաջարկներ</li>
+            <li>All in փաթեթներ</li>
+            <li>Ինտերնետ</li>
+            <li>Հեռախոսակապ</li>
+            <li>Հեռախոսային սպասարկում</li>
+            <li>Գրասենյակներ և ծածկույթ</li>
+            <li>Միջազգային հավաստագրեր</li>
+            <li>Գաղտնիության քաղաքականություն</li>
+          </ul>
+          <ul>
+            <li>Բիզնեսի համար</li>
+            <li>Օպերատորներ</li>
+            <li>Օգնություն</li>
+            <li>Պայմաններ</li>
+            <li>Արխիվ</li>
+            <li>Աշխատատեղեր</li>
+            <li>Այլ հատուկ առաջարկներ</li>
+            <li>Այլ աշխատանքներ</li>
+            <li>Այլ</li>
+          </ul>
+        </div>
       </div>
 
       <div className="footer-section">
@@ -81,7 +83,7 @@ const Footer = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d663.114631263792!2d44.45453626080043!3d40.17462049080225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abdaa42ee6895%3A0xcb433ecac1894517!2zT1ZJTyDQnNCw0LvQsNGC0LjRjyDRgdC10YDQstC40YEt0YbQtdC90YLRgA!5e1!3m2!1sen!2sam!4v1758689085074!5m2!1sen!2sam"
             width="600"
             height="250"
-            style={{ border: 1, borderRadius: "20px"}}
+            style={{ border: 1, borderRadius: "20px" }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
@@ -89,10 +91,10 @@ const Footer = () => {
         </div>
 
         <div className="social-icons">
-          <FaYoutube />
-          <FaFacebook />
-          <FaInstagram />
-          <FaPaperPlane />
+          <FaYoutube className="icon" />
+          <FaFacebook className="icon" />
+          <FaInstagram className="icon" />
+          <FaPaperPlane className="icon" />
         </div>
       </div>
 
